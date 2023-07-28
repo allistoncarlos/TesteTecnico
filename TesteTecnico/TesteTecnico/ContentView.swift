311e3e4308ebf21ebb2ff82ca20e14f26e9f84e9
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-07-28 10:14:13 -0300

    Chore: project creation

---
+++ TesteTecnico/TesteTecnico/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TesteTecnico
+//
+//  Created by Alliston Aleixo on 28/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TesteTecnico/TesteTecnico/TesteTecnicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteTecnicoApp.swift
+//  TesteTecnico
+//
+//  Created by Alliston Aleixo on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TesteTecnicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
